adcc2c08a9d08f337f64cf4d87d0a28b395c9c7e
Iuri Silva <iuriwillian1115@gmail.com>
2025-09-23 16:13:40 -0300

    index criado

---
+++ script.js
@@ -0,0 +1,15 @@
+function mudarTamanho() {
+    if(itens.style.display == "none") {
+        itens.style.display = "block"
+    } else {
+        itens.style.display = "none"
+    }
+}
+
+function formLogin() {
+    if(caixa.style.display == "none") {
+        caixa.style.display = "block"
+    } else {
+        caixa.style.display = "none"
+    }
+}
